4a596d0f636384537becda70bde689c865304036
kwojcik22 <wojcik2k2@gmail.com>
2025-04-20 21:33:14 +0200

    access to baudrate by get_baudrate

--- GUI/GUI.py
+++ GUI/GUI.py
@@ -3,13 +3,8 @@ from tkinter import *
 
 from GUIsrc import get_baudrate
 from GUIsrc import root
-from GUIsrc import baudrate
-from GUIsrc import lbl
 
 tk.Button(root, text="Submit", command=lambda: get_baudrate()).pack(pady=5)
-#var = baudrate
+tk.Button(root, text="check", command=lambda: tk.messagebox.showinfo("Info", f"valid input: {get_baudrate()}")).pack(pady=5)
 
-#tk.Button(root, text="check", command=lambda: tk.messagebox.showerror("Error", f"Invalid input: {baudrate}")).pack(pady=5)
-
-#lbl.config(text=f"Baudrate set to: {baudrate}")  # Update the label with the default baudrate
 root.mainloop()
